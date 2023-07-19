69af9fec7ef533b14d9087ad9e0c56651d74edc4
chaimaefahmi <chaimaefahmi18@gmail.com>
2023-07-18 17:11:44 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@ void jack_bauer(void)
 {
 	int min, hour;
 
-	for (min = 0; min <= 59; min++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (hour = 0; hour <= 23; hour++)
+		for (min = 0; min= 59; min++)
 		{
-			_putchar((min / 10) + 0);
-			_putchar((min % 10) + 0);
-			_putchar(':');
 			_putchar((hour / 10) + 0);
 			_putchar((hour % 10) + 0);
-			_putchar(0);
+			_putchar(':');
+			_putchar((min / 10) + 0);
+			_putchar((min % 10) + 0);
+			_putchar(10);
 		}
 	}
 }
