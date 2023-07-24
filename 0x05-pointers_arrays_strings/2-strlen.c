ed32cb949cb8a83ef133dc65bde5007ea9c13021
chaimaefahmi <chaimaefahmi18@gmail.com>
2023-07-24 14:25:05 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
+int _strlen(char *s)
+{
+	int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
