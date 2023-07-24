219ef572790515e34ae8008654e619d4b431eb37
chaimaefahmi <chaimaefahmi18@gmail.com>
2023-07-24 14:32:52 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-*swap_int - swap two integers
-*@a: first integer
-*@b: second integer
-*Return: nothing
-*/
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
 int _strlen(char *s)
 {
-	int count, inc;
+int count, inc;
 inc = 0;
 for (count = 0; s[count] != '\0'; count++)
 inc++;
