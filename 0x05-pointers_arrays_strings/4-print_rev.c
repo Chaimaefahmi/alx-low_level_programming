e4338a361f59d37a73757edaf963a2d56cb0ff2d
chaimaefahmi <chaimaefahmi18@gmail.com>
2023-07-24 14:42:32 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
 
 void print_rev(char *s)
 {
-int len = 0, index = 0;
-len = _strlen(s);
+	int len = 0, index = 0;
+	len = _strlen(s);
 
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
-_putchar('\n');
+	_putchar('\n');
 }
 
 /**
@@ -22,10 +22,10 @@ _putchar('\n');
  */
 int _strlen(char *s)
 {
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
 
-return (inc);
+	return (inc);
 }
