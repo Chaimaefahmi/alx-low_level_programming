d044bd680c2a7359305388cc8de72006da42c92e
chaimaefahmi <chaimaefahmi18@gmail.com>
2023-08-15 00:31:20 +0000

    helllo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: parameter of name
+ * @age: parameter of age
+ * @owner: parameter to propietary
+ *
+ * Description: Longer descripton
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
