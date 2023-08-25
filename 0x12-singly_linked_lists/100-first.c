510d64dee279fee69acb4beb41d0d6fd16149168
Chaimaefahmi <chaimaefahmi18@gmail.com>
2023-08-25 01:15:56 +0000

    project

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - print a line before the main fuction is executed.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
