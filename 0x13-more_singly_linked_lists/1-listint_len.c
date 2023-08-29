bc20d01fef3a18ae6a76987da92e487a2145d7ab
Chaimaefahmi <chaimaefahmi18@gmail.com>
2023-08-29 21:43:22 +0000

    hello

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,16 +2,18 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
+ *
  * @h: pointer
+ *
  * Return: number of nodes
  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t m;
 
-	while (h)
+	for (m = 0; h != NULL; m++)
 	{
-		m++;
 		h = h->next;
 	}
 	return (m);
