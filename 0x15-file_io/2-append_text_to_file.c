bfd99d66e7e4c47ff204606e6da5ed6ab60434c4
Chaimaefahmi <chaimaefahmi18@gmail.com>
2023-09-05 00:55:45 +0000

    hello

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,6 +6,8 @@
  * @text_content: string
  * Rturn: 1 on success and -1 on failure, If filename is NULL return -1
  * Return 1 if the file exists and -1 if the file does not exist
+ * or if you do not have the required permissions to write the file
+ * If text_content is NULL, do not add anything to the file.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
